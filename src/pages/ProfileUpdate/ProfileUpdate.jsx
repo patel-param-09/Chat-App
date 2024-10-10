1c7b87dc27781aaafd6f1ac65e156c7ec62752e7
patel-param-09 <patelparam9304@gmail.com>
2024-10-10 14:15:15 +0530

    Complete The UI of Profile Page

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Profile.css";
+import assets from "../../assets/assets";
 
 const ProfileUpdate = () => {
-  return <div></div>;
+  const [image, setImage] = useState(false);
+
+  return (
+    <div className="profile">
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input
+              onChange={(e) => {
+                setImage(e.target.files[0]);
+              }}
+              type="file"
+              id="avatar"
+              accept=".png, .jpg, .jpeg"
+              hidden
+            />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.avatar_icon}
+              alt=""
+            />
+            Upload Profile Image
+          </label>
+          <input type="text" placeholder="Your Name" required />
+          <textarea placeholder="Write Profile Bio" required></textarea>
+          <button type="submit">Save</button>
+        </form>
+        <img
+          src={image ? URL.createObjectURL(image) : assets.logo_icon}
+          className="profile-pic"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ProfileUpdate;
